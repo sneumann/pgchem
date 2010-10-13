4a366da236b81e2b47282aae9062dea293849e5f
Steffen Neumann <sneumann(at)ipb-halle.de>
2010-10-13 11:53:37 +0200

    removed old ob release

--- src/openbabel-2.2.3/windows-vc2005/OBCSharp/obdotnet.bat
+++
@@ -1,5 +0,0 @@
-set ver=0.1
-set dist=OBDotNet\bin\release
-copy openbabelcsharp.dll %dist%
-copy ..\*.obf %dist%
-copy ..\*.dll %dist%
--- src/openbabel-2.2.3/windows-vc2005/OBJava/objava.bat
+++
@@ -1,4 +0,0 @@
-"%JAVA_HOME%\bin\javac" org\openbabel\*.java
-del /Q org\openbabel\*.java
-"%JAVA_HOME%\bin\jar" cf openbabel.jar org
-del /Q org\openbabel\*.class
